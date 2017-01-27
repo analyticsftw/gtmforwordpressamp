daaaa526fa58da9c05c6da507256bf6045675cde
Julien Coquet <julien.coquet@gmail.com>
2017-01-27 17:02:50 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,22 +2,8 @@
 /*
 	GTM for Accelerated Mobile Pages (AMP) in Wordpress
 	by Julien Coquet
-	This function outputs a Google Tag Manager snippet and dataLayer adapted for the AMP format, which does not support JavaScript by default.
-	
-	Place this script in your Wordpress theme folder and/or adjust your current theme's functions.php file
-	
-	For more details on Google Tag Manager, visit the Developer Reference at
-	https://developers.google.com/tag-manager/quickstart
-
-  For more details on AMP support in Google Analytics, visit 
-	https://developers.google.com/analytics/devguides/collection/amp-analytics/
-  
-  For more details on AMP support in Google Tag Manager, visit 
-	https://www.simoahava.com/analytics/accelerated-mobile-pages-via-google-tag-manager/
-    
-	You can extend this function by leveraging other contextual Wordpress elements and functions.
-	Refer to the Wordpress Codex (knowledge base) at
-	https://codex.wordpress.org/Developer_Documentation
+	This function outputs a Google Tag Manager snippet and dataLayer adapted for the AMP format, 
+	which does not support JavaScript by default.
 */
 
 //AMP Analytics
